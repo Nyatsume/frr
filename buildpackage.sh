6957298ef3016901024f6176c3d3a26f156ca820
Yasuhiro Ohara <yasu@nttv6.jp>
2023-06-02 22:35:49 +0900

    stop username hardcoding.

--- buildpackage.sh
+++ buildpackage.sh
@@ -1,3 +1,3 @@
-sudo chown -R yasu .
+sudo chown -R $USER .
 rm -rf build
 sudo gbp buildpackage --git-ignore-new --git-builder=dpkg-buildpackage --git-debian-branch="$(git rev-parse --abbrev-ref HEAD)" -uc -us --build=any
